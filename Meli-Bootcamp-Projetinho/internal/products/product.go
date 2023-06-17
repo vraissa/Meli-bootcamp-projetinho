ebceeb806b8d082bd6ab657e6901ab1ed9ba5d1e
Vitoria Raissa Zeferino <90735412+vraissa@users.noreply.github.com>
2023-06-16 21:32:30 -0300

    mais ajustes

--- Meli-Bootcamp-Projetinho/internal/products/product.go
+++
@@ -1,9 +0,0 @@
-package products
-
-type Product struct {
-	ID       int     `json:"id"`
-	Name     string  `json:"name"`
-	Category string  `json:"category"`
-	Count    int     `json:"count"`
-	Price    float64 `json:"price"`
-}
